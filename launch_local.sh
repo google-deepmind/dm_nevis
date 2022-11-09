bca545acda5372f3444ea796be57c791b5f1d760
Arthur Douillard <douillard@google.com>
2022-11-09 15:00:20 +0000

    Mount the tensorflow datasets directory.
    Thus if the user already have TFDS datasets they can be directly used in the docker.
    
    PiperOrigin-RevId: 487232288
    Change-Id: I50feeba05680f0e2537b5704fc847f1395755ac7

--- launch_local.sh
+++ launch_local.sh
@@ -8,11 +8,16 @@ OPTIONS="${@:3}"
 eval LSCL_DATA_DIR="~/nevis/nevis_data_dir/stable"
 eval LSCL_TENSORBOARD_DIR="~/nevis/tensorboard"
 
+if [ -z "${TFDS_DATA_DIR}" ]; then
+    eval TFDS_DATA_DIR='~/tensorflow_datasets'
+fi
+
 IMG="nevis-${FRAMEWORK}"
 docker build -f dockerfile.${FRAMEWORK} . -t "$IMG" --network=host
 
 docker run \
--v ${LSCL_DATA_DIR}:/tmp/nevis_data_dir \
--v ${LSCL_TENSORBOARD_DIR}:/tmp/tensorboard \
--it "$IMG" \
---config=./experiments_${FRAMEWORK}/configs/${CONFIG}.py ${OPTIONS}
+  -v ${TFDS_DATA_DIR}:/app/tensorflow_datasets \
+  -v ${LSCL_DATA_DIR}:/tmp/nevis_data_dir \
+  -v ${LSCL_TENSORBOARD_DIR}:/tmp/tensorboard \
+  -it "$IMG" \
+  --config=./experiments_${FRAMEWORK}/configs/${CONFIG}.py ${OPTIONS}
