abc4d308b14d17a02599f53de675d5f8cc3e669d
Arthur Douillard <douillard@google.com>
2022-11-11 15:32:19 +0000

    Fix entended_yaleb.
    The dataset urls don't exist anymore.
    But the dataset was existing in Kaggle. I've checked that the internal files are the same.
    
    PiperOrigin-RevId: 487818053
    Change-Id: Iffe5a999c4e89321e0194fdf28a086e4659a8d19

--- dm_nevis/datasets_storage/handlers/extended_yaleb.py
+++ dm_nevis/datasets_storage/handlers/extended_yaleb.py
@@ -16,11 +16,10 @@
 
 import os
 from typing import List
+import zipfile
 
 from dm_nevis.datasets_storage.handlers import extraction_utils as utils
 from dm_nevis.datasets_storage.handlers import types
-
-from tensorflow.io import gfile
 
 
 _POSES = {
@@ -37,30 +36,12 @@ _NUM_CLASSES = 28
 _FILES_ID_RANGE = (11, 40)
 _MISSING_ID = 14
 
-MD5 = [
-    '9f500ec230e42dd306b9813b86890b50', 'ecc584a42165338ab4691272c74c89be',
-    '7898e82336213e9880673826bab51984', '3332d7868f1f2654380be70029f1acbe',
-    '5f6d1435eb5c379b489750df38c5ba0a', 'c9b6a6943cb1ecebc17c3d3c371c2b13',
-    '5cc4aeb2e9fcc515302d0ae60445a332', 'c3bb2594b056c33e747b59128bd60f5b',
-    '29c028ed8fc77f13a22fcc49cb00ad8a', 'd07e0cb25c50fe33bfbc95912eb8e156',
-    'd46dc7bc85268e4ea7460a0262df1a9a', '3c2425f1dfb360ca89bd7f757e54c116',
-    '73441bab2a59863bdc115c2905477754', '0e858e48bf52cfc7911c4ec618c744fa',
-    'f89f08063899dc7da55116d87adc1eb0', '8f634995b0ebc1efcf61899082dec45a',
-    '47c54d73d68b9b65e1f26636bef4f8f2', 'b10779da543b92123feff08892f8aa56',
-    '096750421595332280880a46793d6aa7', '5811503cdf3a7bb1595d0431e6ebd4f8',
-    'e8f3d4bc753b61714113fdef6d2c03f1', 'd02672184c458fd0a694731c5343abae',
-    '1a416278420d7ea76101e870699c8a85', '5c3c3a3fc06be8bbe7337b586a3f4050',
-    '8220f834edcba9f889daae0c60d23cca', 'e305481abddfd0a7e87d1a3b32b25622',
-    '5d0b9285844743113afb022fa46d1072', '7d68d2d6b396d36bd4f61032553c242a'
-]
 
-
-def _get_all_class_names(filenames: List[str]) -> List[str]:
-  names = []
-  for fname in filenames:
-    fields = fname.split('.')
-    if len(fields) > 1 and fields[1] == 'zip':
-      names.append(fields[0])
+def _get_all_class_names(directories: List[str]) -> List[str]:
+  names = set()
+  for fname in directories:
+    fname = fname.split('/')[-2]
+    names.add(fname)
   return sorted(names)
 
 
@@ -93,13 +74,16 @@ def extended_yaleb_handler(dataset_path: str) -> types.HandlerOutput:
   Returns:
     Metadata and generator functions.
   """
+  zip_name = os.path.join(dataset_path, 'extended-yale-dataset-b.zip')
+  with zipfile.ZipFile(zip_name, 'r') as z:
+    directories = z.namelist()
+    class_names = _get_all_class_names(directories)
 
-  filenames = gfile.listdir(dataset_path)
-  class_names = _get_all_class_names(filenames)
-  assert len(class_names) == _NUM_CLASSES
-  label_str_to_int = {}
-  for int_id, subject_id in enumerate(class_names):
-    label_str_to_int[subject_id] = int_id
+    assert len(class_names) == _NUM_CLASSES, (len(class_names), _NUM_CLASSES)
+    label_str_to_int = {}
+    for int_id, subject_id in enumerate(class_names):
+      label_str_to_int[subject_id] = int_id
+
   metadata = types.DatasetMetaData(
       num_channels=1,
       num_classes=len(class_names),
@@ -129,7 +113,7 @@ def extended_yaleb_handler(dataset_path: str) -> types.HandlerOutput:
 
     return utils.generate_images_from_zip_files(
         dataset_path=dataset_path,
-        zip_file_names=filenames,
+        zip_file_names=[zip_name],
         path_to_label_fn=path_to_label,
         ignored_files_regex=_IGNORED_FILES_REGEX,
         path_filter=path_filter_fn)
@@ -141,19 +125,13 @@ def extended_yaleb_handler(dataset_path: str) -> types.HandlerOutput:
   return metadata, per_split_gen
 
 
-file_list = []
-prefix = 'http://vision.ucsd.edu/extyaleb/ExtendedYaleBZip/yaleB'
-j = 0
-
-for i in range(_FILES_ID_RANGE[0], _FILES_ID_RANGE[1]):
-  if i != _MISSING_ID:  # Missing id.
-    file_list.append(
-        (types.DownloadableArtefact(url=f'{prefix}_{i}.zip', checksum=MD5[j])))
-    j += 1
-
 extended_yaleb_dataset = types.DownloadableDataset(
     name='extended_yaleb',
-    download_urls=file_list,
+    download_urls=[
+        types.KaggleDataset(
+            dataset_name='souvadrahati/extended-yale-dataset-b',
+            checksum='ef37284be91fe0c81dcd96baa948a2db')
+    ],
     handler=extended_yaleb_handler,
     paper_title='Acquiring Linear Subspaces for Face Recognition under Variable Lighting',
     authors='Kuang-Chih Lee, Jeffrey Ho, and David Kriegman',
